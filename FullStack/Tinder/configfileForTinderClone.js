383eeb895bc3e809a6318263b893a944c0166f69
Niti Shah <emailtonitishah@gmail.com>
2021-10-20 19:01:14 +0530

    adding all web development projects

---
+++ FullStack/Tinder/configfileForTinderClone.js
@@ -0,0 +1,9 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyDkzi1KoPzcrwTNdX9n22072vY0TyLv_iY",
+    authDomain: "tinder-clone-a6697.firebaseapp.com",
+    projectId: "tinder-clone-a6697",
+    storageBucket: "tinder-clone-a6697.appspot.com",
+    messagingSenderId: "672171192754",
+    appId: "1:672171192754:web:86efd8742573e56e7474ab",
+    measurementId: "G-S227RSPVZG"
+  };
